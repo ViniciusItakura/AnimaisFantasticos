c1e6584f386eeca6c346fc364f2e4e35cddc0e4c
ViniciusItakura <viniciusitakura1@gmail.com>
2023-05-24 13:17:37 -0300

    Criado scroll suave com JavaScript

--- script.js
+++ script.js
@@ -24,20 +24,40 @@ initTabNav();
 
 
 function initAccordion() {
-const accordionList = document.querySelectorAll('.js-accordion dt');
-if(accordionList.length){
-  accordionList[0].classList.add('ativo');
-  accordionList[0].nextElementSibling.classList.add('ativo');
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  if(accordionList.length){
+    accordionList[0].classList.add('ativo');
+    accordionList[0].nextElementSibling.classList.add('ativo');
 
-  function activeAccordion() {
-    this.classList.toggle('ativo');
-    this.nextElementSibling.classList.toggle('ativo');
-  }
+    function activeAccordion() {
+      this.classList.toggle('ativo');
+      this.nextElementSibling.classList.toggle('ativo');
+    }
 
-  accordionList.forEach((item) => {
-    item.addEventListener('click', activeAccordion);
-  });
-  }
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion);
+    });
+    }
 }
-
 initAccordion();
+
+
+function initScrollSuave(){
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
+initScrollSuave();
