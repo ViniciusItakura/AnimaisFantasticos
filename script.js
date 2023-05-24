f84f79878f478137ffd41762a856f29c167ab29e
ViniciusItakura <viniciusitakura1@gmail.com>
2023-05-24 13:01:25 -0300

    Adicionado Menu Accordion com JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+function initTabNav() {
+
 const tabMenu = document.querySelectorAll('.js-tabmenu li');
 const tabContent = document.querySelectorAll('.js-tabcontent section');
 
@@ -17,3 +19,25 @@ if(tabMenu.length && tabContent.length){
     });
   });
 }
+}
+initTabNav();
+
+
+function initAccordion() {
+const accordionList = document.querySelectorAll('.js-accordion dt');
+if(accordionList.length){
+  accordionList[0].classList.add('ativo');
+  accordionList[0].nextElementSibling.classList.add('ativo');
+
+  function activeAccordion() {
+    this.classList.toggle('ativo');
+    this.nextElementSibling.classList.toggle('ativo');
+  }
+
+  accordionList.forEach((item) => {
+    item.addEventListener('click', activeAccordion);
+  });
+  }
+}
+
+initAccordion();
